9fc05c7121505ba0cdc23bd0f70b55bb2c8e4cde
Jared Brown <jared@Jareds-MBP.lan1>
2019-12-12 23:35:31 -0800

    step 3 and 4 complete

--- user-onboarding/src/Components/Form.js
+++ user-onboarding/src/Components/Form.js
@@ -4,6 +4,12 @@ import * as Yup from "yup";
 import axios from "axios";
 
 const Forms = ({ values, errors, touched, status }) => {
+  const [onBoard, setOnBoard] = useState([]);
+
+  useEffect(() => {
+    status && setOnBoard(onBoard => [...onBoard, status]);
+  }, [status]);
+
   return (
     <div>
       <Form>
@@ -23,9 +29,18 @@ const Forms = ({ values, errors, touched, status }) => {
           <p className="errors">{errors.password}</p>
         )}
         <label>Terms Of Service</label>
-        <Field type="checkbox" name="terms" />
+        <Field type="checkbox" name="terms" checked={values.terms} />
         <button type="submit">Submit</button>
       </Form>
+      {onBoard.map(board => {
+        return (
+          <ul key={board.id}>
+            <li>Name: {board.name}</li>
+            <li>Email: {board.email}</li>
+            <li>Password: {board.password}</li>
+          </ul>
+        );
+      })}
     </div>
   );
 };
@@ -43,7 +58,16 @@ const FormikOnBoardForm = withFormik({
     name: Yup.string().required("Name is required"),
     email: Yup.string().required("Email is required"),
     password: Yup.string().required("Password is required")
-  })
+  }),
+  handleSubmit(values, { setStatus }) {
+    axios
+      .post("https://reqres.in/api/users", values)
+      .then(res => {
+        console.log("success", res);
+        setStatus(res.data);
+      })
+      .catch(err => console.log(err.response));
+  }
 })(Forms);
 
 export default FormikOnBoardForm;
